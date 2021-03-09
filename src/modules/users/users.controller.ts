ccc44533719ad4658a2c5e77026d52ad9dc89c4e
MaksonBurkov <mack.nikitin@gmail.com>
2021-03-09 16:23:19 +0300

    09/03/21

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { User } from './models/user.entity';
+import { UsersService } from './users.service';
+import { Repository } from 'typeorm';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  getUsers(): string {
+    return this.usersService.findAll();
+  }
+}
